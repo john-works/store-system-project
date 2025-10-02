ffb3feb2dd1c721036f4cec0058c28df7f470533
john-works <workjohn265@gmail.com>
2025-10-02 10:39:15 +0300

    New Changes

--- resources/views/moverments/show.blade.php
+++ resources/views/moverments/show.blade.php
@@ -1,7 +1,9 @@
-{{-- @extends('layouts.public')
+@extends('layouts.public')
 
 @section('content')
 
+<div id="app">
+    <div id="main">
         <header class="mb-3">
             <a href="#" class="burger-btn d-block d-xl-none">
                 <i class="bi bi-justify fs-3"></i>
@@ -12,87 +14,82 @@
             <div class="page-title">
                 <div class="row">
                     <div class="col-12 col-md-6 order-md-1 order-last">
-                        <h3>Invoice Details</h3>
+                        <!-- Left side title if needed -->
                     </div>
                     <div class="col-12 col-md-6 order-md-2 order-first">
                         <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
                             <ol class="breadcrumb">
                                 <li class="breadcrumb-item"><a href="">Dashboard</a></li>
-                                <li class="breadcrumb-item"><a href="{{ route('invoices.index') }}">Invoices</a></li>
-                                <li class="breadcrumb-item active" aria-current="page">View Invoice</li>
+                                <li class="breadcrumb-item active" aria-current="page">Movement</li>
                             </ol>
                         </nav>
                     </div>
                 </div>
             </div>
 
-            <!-- Show Invoice -->
-            <section id="view-invoice">
+            <!-- Show Movement Details -->
+            <section id="movement-details">
                 <div class="row match-height">
                     <div class="col-12">
-                        <div class="card shadow-lg border-0">
-                            <div class="card-header bg-primary text-white">
-                                {{-- <h4 class="card-title">Invoice #{{ $invoice->invoice_number }}</h4> --
+                        <div class="card">
+                            <div class="card-header">
+                                <h4 class="card-title">Movement Details</h4>
                             </div>
-                            <div class="card-body">
-                                <table class="table table-bordered table-striped">
-                                    <tbody>
+                            <div class="card-content">
+                                <div class="card-body">
+                                    <table class="table table-striped">
                                         <tr>
-                                            <th>Supplier Name</th>
-                                            <td>{{ $invoice->supplier_name }}</td>
+                                            <th>Item</th>
+                                            <td>{{ $movement->item->item_name ?? 'N/A' }}</td>
                                         </tr>
                                         <tr>
-                                            <th>Received Date</th>
-                                            <td>{{ \Carbon\Carbon::parse($invoice->received_date)->format('d M, Y') }}</td>
+                                            <th>From Department</th>
+                                            <td>{{ $movement->from_department }}</td>
                                         </tr>
                                         <tr>
-                                            <th>Invoice Number</th>
-                                            <td>{{ $invoice->invoice_number }}</td>
+                                            <th>From User</th>
+                                            <td>{{ $movement->fromUser->name ?? 'N/A' }}</td>
                                         </tr>
                                         <tr>
-                                            <th>Description</th>
-                                            <td>{{ $invoice->invoice_description }}</td>
+                                            <th>To Department</th>
+                                            <td>{{ $movement->to_department }}</td>
                                         </tr>
                                         <tr>
-                                            <th>Invoice Date</th>
-                                            <td>{{ \Carbon\Carbon::parse($invoice->invoice_date)->format('d M, Y') }}</td>
+                                            <th>To User</th>
+                                            <td>{{ $movement->toUser->name ?? 'N/A' }}</td>
                                         </tr>
                                         <tr>
-                                            <th>Received Amount</th>
-                                            <td>{{ number_format($invoice->received_amount, 2) }}</td>
+                                            <th>Request Summary</th>
+                                            <td>{{ $movement->request_summary }}</td>
                                         </tr>
                                         <tr>
-                                            <th>Currency</th>
-                                            <td>{{ $invoice->invoice_currency }}</td>
+                                            <th>Request Date</th>
+                                            <td>{{ $movement->request_date }}</td>
                                         </tr>
-                                    </tbody>
-                                </table>
-
-                                <div class="mt-4 d-flex justify-content-between">
-                                    <a href="{{ route('invoices.index') }}" class="btn btn-secondary">
-                                        <i class="bi bi-arrow-left"></i> Back
-                                    </a>
-                                    <div>
-                                        <a href="{{ route('invoices.edit', $invoice->id) }}" class="btn btn-warning">
-                                            <i class="bi bi-pencil-square"></i> Edit
-                                        </a>
-                                        <form action="{{ route('invoices.destroy', $invoice->id) }}" method="POST" class="d-inline">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-danger"
-                                                onclick="return confirm('Are you sure you want to delete this invoice?')">
-                                                <i class="bi bi-trash"></i> Delete
-                                            </button>
-                                        </form>
-                                    </div>
+                                        <tr>
+                                            <th>Requested By</th>
+                                            <td>{{ $movement->request_by }}</td>
+                                        </tr>
+                                    </table>
                                 </div>
-
+                                <div class="card-footer">
+                                    <a href="{{ route('moverments.index') }}" class="btn btn-secondary">Back</a>
+                                    <a href="{{ route('moverments.edit', $movement->id) }}" class="btn btn-warning">Edit</a>
+                                    <form action="{{ route('moverments.destroy', $movement->id) }}" method="POST" style="display:inline;">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger"
+                                            onclick="return confirm('Are you sure you want to delete this movement?')">Delete</button>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
-            <!-- End Show Invoice -->
+
         </div>
-    
-@endsection --}}
+    </div>
+</div>
+
+@endsection

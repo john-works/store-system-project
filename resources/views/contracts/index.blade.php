a111737a535181456f7ffeaeba374ac40a135677
john-works <workjohn265@gmail.com>
2025-10-08 14:17:18 +0300

    New Changes

--- resources/views/contracts/index.blade.php
+++ resources/views/contracts/index.blade.php
@@ -3,14 +3,6 @@
 
 @section('content')
 
-{{-- 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DataTable </title>
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
@@ -22,12 +14,10 @@
     <link rel="stylesheet" href="assets/vendors/bootstrap-icons/bootstrap-icons.css">
     <link rel="stylesheet" href="assets/css/app.css">
     <link rel="shortcut icon" href="assets/images/favicon.svg" type="image/x-icon">
-</head> --}}
 
-{{-- <body>
     <div id="app">
         
- --}}
+
 
         <div id="main">
             <header class="mb-3">
